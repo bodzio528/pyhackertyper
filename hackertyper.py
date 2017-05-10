b9cd95e338dd2760f974fe5829b9854b4a78ddcb
Bogumił Chojnowski <bogumil.chojnowski@gmail.com>
2017-05-10 21:42:05 +0200

    rewrite to simplified pattern
    Signed-off-by: Bogumił Chojnowski <bogumil.chojnowski@gmail.com>

--- hackertyper.py
+++ hackertyper.py
@@ -1,135 +1,122 @@
 #! /usr/bin/python3
 
-import threading
 import unittest
 import unittest.mock
 
 
-class Collector(object):
-    def __init__(self, local_clock):
-        self.collected = []
-        self.local_clock = local_clock
-
-    def collect(self, char):
-        self.collected.append((char, self.local_clock.timestamp()))
+def current_timestamp_ms():
+    import time
+    return int(round(time.time() * 1000))
 
 
-class CollectorTest(unittest.TestCase):
+def collect(char, buffer):
+    buffer.append((char, current_timestamp_ms()))
+
+
+class CollectTest(unittest.TestCase):
     def setUp(self):
-        self.local_clock = unittest.mock.MagicMock()
-        self.local_clock.timestamp.return_value = 0
-        self.sut = Collector(self.local_clock)
+        self.buffer = []
 
-    def test_collector_can_collect_characters(self):
-        self.sut.collect('c')
-        self.assertIn(('c', 0), self.sut.collected)
-
-    def test_collector_adds_timestamp_during_collection(self):
-        self.local_clock.timestamp.return_value = 10
-        self.sut.collect('c')
-        self.assertIn(('c', 10), self.sut.collected)
+    @unittest.mock.patch('hackertyper.current_timestamp_ms')
+    def test_collect_uses_current_timestamp_ms(self, current_timestamp):
+        current_timestamp.return_value = 0xDEAD
+        collect('c', self.buffer)
+        self.assertIn(('c', 0xDEAD), self.buffer)
 
 
-class Hackertyper(object):
-    def __init__(self, buffer, collector, inactivity_timer):
-        self.buffer = buffer
-        self.collector = collector
-        self.inactivity_timer = inactivity_timer
+def validate(char, input_buffer, expected_buffer):
+    return char == expected_buffer[len(input_buffer)]
 
-    def receive(self, char):
-        self.inactivity_timer.reset()
-        if self.buffer.check_character(char):
-            self.collector.collect(char)
+
+class ValidateTest(unittest.TestCase):
+    def test_validate_check_compares_next_expected_char_with_provided(self):
+        input_buffer = []
+        expected_buffer = ['l', 'o']
+        self.assertTrue(validate('l', input_buffer, expected_buffer))
+
+
+def user_input():
+    return 'a'
+
+
+def hackertyper(challenge_text):
+    errors = 0
+    input_buffer = []
+    while len(input_buffer) < len(challenge_text):
+        ch = user_input()
+        if not validate(ch, input_buffer, challenge_text):
+            errors += 1
+        collect(ch, input_buffer)
+
+    return {'errors': errors, 'total': len(challenge_text), 'collected': input_buffer}
 
 
 class HackertyperTest(unittest.TestCase):
     def setUp(self):
-        self.buffer = unittest.mock.MagicMock()
-        self.buffer.check_character.return_value(True)
+        self.expected_text = 'some sequence of characters'
+        self.user_text_1 = 'some saquence of characters'
+        self.user_text_2 = 'some saquence of cheracters'
 
-        self.collector = unittest.mock.MagicMock()
+    def test_hackertyper_returns_summary(self):
+        summary = hackertyper('')
+        self.assertIn('errors', summary)
+        self.assertIn('total', summary)
 
-        self.inactivity_timer = unittest.mock.MagicMock()
+    def test_hackertyper_returns_total_number_of_characters_expected(self):
+        summary = hackertyper('asdf')
+        self.assertEqual(summary['total'], 4)
 
-        self.sut = Hackertyper(self.buffer, self.collector, self.inactivity_timer)
+    @unittest.mock.patch('hackertyper.user_input')
+    def test_no_errors_for_perfectly_matched_input(self, patched_user_input):
+        patched_user_input.side_effect = list(self.expected_text)
+        summary = hackertyper(self.expected_text)
+        self.assertEqual(summary['errors'], 0)
 
-    def test_hackertyper_collect_only_if_valid_input(self):
-        self.sut.receive('c')
-        self.buffer.check_character.assert_called_with('c')
-        self.collector.collect.assert_called_with('c')
+    @unittest.mock.patch('hackertyper.user_input')
+    def test_one_error_for_input_with_one_typo(self, patched_user_input):
+        patched_user_input.side_effect = list(self.user_text_1)
+        summary = hackertyper(self.expected_text)
+        self.assertEqual(summary['errors'], 1)
 
-    def test_hackertyper_discard_if_invalid_input(self):
-        self.buffer.check_character.return_value = False
-        self.sut.receive('c')
-        self.buffer.check_character.assert_called_with('c')
+    @unittest.mock.patch('hackertyper.user_input')
+    def test_one_error_for_input_with_two_typos(self, patched_user_input):
+        patched_user_input.side_effect = list(self.user_text_2)
+        summary = hackertyper(self.expected_text)
+        self.assertEqual(summary['errors'], 2)
 
-    def test_hackertyper_received_input_resets_inactivity_timer(self):
-        self.sut.receive('c')
-        self.inactivity_timer.reset.assert_called_once_with()
+    @unittest.mock.patch('hackertyper.user_input')
+    @unittest.mock.patch('hackertyper.current_timestamp_ms')
+    def test_hackertyper_returns_collected_data(self, patched_current_timestamp, patched_user_input):
+        patched_user_input.side_effect = list(self.user_text_2)
+        patched_current_timestamp.return_value = 0
+        summary = hackertyper(self.expected_text)
+        self.assertEqual(summary['collected'], list(zip(self.user_text_2, [0] * len(self.user_text_2))))
 
-
-class InactivityTimer(object):
-    def __init__(self, interval, timeout_handler):
-        self.function = timeout_handler
-        self.interval = interval
-        self._timer = None
-        self._is_running = False
-
-    def start(self):
-        self._is_running = True
-        self._timer = threading.Timer(self.interval, self.function)
-        self._timer.start()
-
-    def stop(self):
-        if self._is_running:
-            self._timer.cancel()
-            self._is_running = False
-
-    def reset(self):
-        self.stop()
-        self.start()
-
-
-class InactivityTimerTest(unittest.TestCase):
-    def setUp(self):
-        pass
-
-    @unittest.skip('Time costly')
-    def test_can_be_restarted(self):
-        obj = unittest.mock.MagicMock()
-        sut = InactivityTimer(interval=1, timeout_handler=obj.timeout_handler)
-        sut.reset()
-        sut.reset()
-        from time import sleep
-        sleep(1)
-        obj.timeout_handler.assert_called_once_with()
+    @unittest.mock.patch('hackertyper.user_input')
+    @unittest.mock.patch('hackertyper.current_timestamp_ms')
+    def test_hackertyper_returns_collected_data_timestamp_progress(self, patched_current_timestamp, patched_user_input):
+        patched_user_input.side_effect = list(self.user_text_2)
+        patched_current_timestamp.side_effect = range(0, 1000, 20)
+        summary = hackertyper(self.expected_text)
+        self.assertEqual(summary['collected'], list(zip(self.user_text_2, range(0, 1000, 20))))
 
 
-class LocalClock(object):
-    def __init__(self):
-        self._paused = True
+def print_summary(summary):
+    errors = summary['errors']
+    total = summary['total']
+    error_rate = 100.0 * errors / total
+    end_time = summary['collected'][-1][1]
+    start_time = summary['collected'][0][1]
+    duration = round((end_time - start_time) / 1000.0, 4)
+    if duration != 0:
+        avg_speed = (total - errors) / duration
+    else:
+        avg_speed = 'infinity'
 
-    def pause(self):
-        pass
+    print('Errors {0}({1}%) Avg. Speed {2}'.format(errors, error_rate, avg_speed))
 
-    def timestamp(self):
-        return 0
-
-
-class LocalClockTest(unittest.TestCase):
-    def setUp(self):
-        self.sut = LocalClock()
-
-    def test_local_clock_can_be_paused_on_request(self):
-        self.sut.pause()
-
-    def test_local_clock_first_timestamp_is_always_zero(self):
-        self.assertEqual(self.sut.timestamp(), 0)
 
 if __name__ == '__main__':
-    pass
-    # buffer = Buffer()
-    # local_clock = LocalClock()
-    # inactivity_timer = InactivityTimer(5, local_clock.pause)
-    # collector = Collector(local_clock)
-    # hackertyper = Hackertyper(buffer, collector, inactivity_timer)
+    expected_text = 'some text'
+    print(expected_text)
+    print_summary(hackertyper(expected_text))
